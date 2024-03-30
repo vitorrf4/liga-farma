df829c8b0935539fb782cda90893295bde5558bc
vitor <vitorrf4@gmail.com>
2024-03-30 17:11:16 -0300

    adiciona contrato model

---
+++ backend/models/Contrato.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/sequelize');
+const farmacia = require('./Farmacia');
+const farmaceutico = require('./Farmaceutico');
+
+const Contrato = sequelize.define('Contrato', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        cpf: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dataInicio: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dataFim: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        }},
+    { timestamps: false });
+
+Contrato.belongsTo(farmacia, {
+    as: "farmacia_id",
+    foreignKey: {
+        allowNull: false
+    }
+});
+Contrato.belongsTo(farmaceutico, {
+    as: "farmaceutico_id",
+    foreignKey: {
+        allowNull: false
+    }
+});
+
+Contrato.sync(); // Sincroniza o modelo com o banco de dados
+
+module.exports = Contrato;
