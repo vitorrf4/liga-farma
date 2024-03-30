1e87147771560039ef64e20f2c7a5b586234f2fa
vitor <vitorrf4@gmail.com>
2024-03-30 12:39:33 -0300

    cria tabelas do sqlite

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express()
 const port = process.env.PORT;
-const db = require('./databases/db.js');
+require('./databases/db.js');
 
 app.listen(port, () => {
     console.log(`Servidor iniciado na porta ${port}`);
