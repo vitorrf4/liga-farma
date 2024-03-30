580aa0258c8af85685ba976e5a8752407a8018f7
vitor <vitorrf4@gmail.com>
2024-03-30 16:02:58 -0300

    atualiza estrutura da pasta

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 3000;
-require('./sequelize');
+require('./database/sequelize');
 
 app.use(express.json());
 
--- models/Farmaceutico.js
+++ models/Farmaceutico.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../sequelize');
+const sequelize = require('../database/sequelize');
 
 const Farmaceutico = sequelize.define('Farmaceutico', {
         id: {
