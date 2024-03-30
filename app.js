2c9dc6db5454dec3c059450821a5bd495cad98d1
vitor <vitorrf4@gmail.com>
2024-03-30 11:45:36 -0300

    adiciona conexao com sqlite

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express()
 const port = process.env.PORT;
+const db = require('./databases/db.js');
 
 app.listen(port, () => {
     console.log(`Servidor iniciado na porta ${port}`);
 });
-
