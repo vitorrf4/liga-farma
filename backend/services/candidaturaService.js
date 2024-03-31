8e74fd3d7353ef5ad8dbcd3ef0ce955446b56ac1
vitor <vitorrf4@gmail.com>
2024-03-31 16:49:58 -0300

    cria cnadidatura service

---
+++ backend/services/candidaturaService.js
@@ -0,0 +1,44 @@
+const Candidatura = require('../models/Candidatura');
+
+class CandidaturaService {
+    async getCandidatura() {
+        return await Candidatura.findAll();
+    }
+
+    async getCandidaturaPorId(id) {
+        const candidatura = await Candidatura.findOne({ where: { id: id } });
+
+        if (!candidatura) {
+            return null;
+        }
+
+        return candidatura;
+    }
+
+    async cadastrarCandidatura(vagaId, farmaceuticoId, mensagem) {
+        return Candidatura.create({vagaId, farmaceuticoId, mensagem});
+    }
+
+    async atualizarCandidatura(candidatura) {
+        const candidaturaDb = await this.getCandidaturaPorId(candidatura.id);
+
+        if (!candidaturaDb) {
+            return false;
+        }
+
+        await Candidatura.update(
+            candidatura,
+            { where: { id: candidatura.id } }
+        );
+
+        return true;
+    }
+
+    async deletarCandidatura(id) {
+        return Candidatura.destroy({
+            where: { id: id }
+        });
+    }
+}
+
+module.exports = new CandidaturaService();
