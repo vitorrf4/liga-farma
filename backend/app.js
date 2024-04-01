34997e7c617947fae9ecfecec465caaa645c212f
vitor <vitorrf4@gmail.com>
2024-04-01 11:26:48 -0300

    separa ambientes em pastas

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const env = process.env.NODE_ENV || 'development';
-require('dotenv').config({ path: `.env.${env}`});
+require('dotenv').config({ path: `./environments/.env.${env}`});
 const port = process.env.port || 3000;
 require('./database/sequelize');
 
