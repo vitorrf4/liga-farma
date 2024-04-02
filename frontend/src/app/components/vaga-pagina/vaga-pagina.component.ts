44472fcb53c8882507dfb89f77b3927ff278e346
vitor <vitorrf4@gmail.com>
2024-04-02 00:34:08 -0300

    adiciona componente de vagas detalhadas

---
+++ frontend/src/app/components/vaga-pagina/vaga-pagina.component.ts
@@ -0,0 +1,21 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Vaga} from "../../models/vaga";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-vaga-pagina',
+  standalone: true,
+  imports: [
+    NgIf
+  ],
+  templateUrl: './vaga-pagina.component.html',
+  styleUrl: './vaga-pagina.component.css'
+})
+export class VagaPaginaComponent {
+  @Input() vaga: Vaga | null = null;
+  @Output() vagaDeselecionadaEvent = new EventEmitter();
+
+  deselecionarVaga() {
+    this.vagaDeselecionadaEvent.emit();
+  }
+}
