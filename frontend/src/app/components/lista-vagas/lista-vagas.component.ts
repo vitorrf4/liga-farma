173ec30f11225aaa9f13b225bf28355380f8d2fa
vitor <vitorrf4@gmail.com>
2024-04-02 00:33:32 -0300

    adiciona tela de vaga detalhada

--- frontend/src/app/components/lista-vagas/lista-vagas.component.ts
+++ frontend/src/app/components/lista-vagas/lista-vagas.component.ts
@@ -1,21 +1,25 @@
 import {Component, OnInit} from '@angular/core';
 import {Vaga} from "../../models/vaga";
 import {VagaService} from "../../services/vaga.service";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {RouterLink} from "@angular/router";
+import {VagaPaginaComponent} from "../vaga-pagina/vaga-pagina.component";
 
 @Component({
   selector: 'app-lista-vagas',
   standalone: true,
   imports: [
     NgForOf,
-    RouterLink
+    RouterLink,
+    VagaPaginaComponent,
+    NgIf
   ],
   templateUrl: './lista-vagas.component.html',
   styleUrl: './lista-vagas.component.css'
 })
 export class ListaVagasComponent implements OnInit {
   vagas: Vaga[] = [];
+  vagaSelecionada: Vaga | null = null;
 
   constructor(private service: VagaService) { }
 
@@ -23,5 +27,13 @@ export class ListaVagasComponent implements OnInit {
     this.service.listar().subscribe(res => {
       this.vagas = res;
     });
+  }
+
+  mudarVagaSelecionada(vaga: Vaga) {
+    this.vagaSelecionada = vaga;
+  }
+
+  deselecionarVaga() {
+    this.vagaSelecionada = null;
   }
 }
