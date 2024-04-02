c2c778f06b3a8fde8268e40fab2e6a301073be22
vitor <vitorrf4@gmail.com>
2024-04-02 11:12:44 -0300

    adiciona form base de cadastro

--- frontend/src/app/components/cadastro-pessoa/cadastro-pessoa.component.ts
+++ frontend/src/app/components/cadastro-pessoa/cadastro-pessoa.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-cadastro-farmaceutico',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe
+  ],
   templateUrl: './cadastro-pessoa.component.html',
   styleUrl: './cadastro-pessoa.component.css'
 })
 export class CadastroPessoaComponent {
+  form!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  get debug() {return this.form.invalid};
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: ["", Validators.required],
+      senha: ["", Validators.required],
+      nome: ["", Validators.required],
+      telefone: ["", Validators.required],
+      endereco: this.formBuilder.group({
+        rua: ["", Validators.required],
+        numero: ["", Validators.required],
+        cep: ["", Validators.required],
+        complemento: [""],
+        regiao: this.formBuilder.group({
+          nome: ["", Validators.required]
+        })
+      })
+    });
+  }
+
+  cadastrarCliente() {
+
+  }
 }
