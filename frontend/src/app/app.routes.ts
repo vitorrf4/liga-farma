df0de40144b439ce8f03060af75a1bded819b308
vitor <vitorrf4@gmail.com>
2024-04-01 23:11:32 -0300

    adiciona vaga component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {FarmaceuticoComponent} from "./components/farmaceutico/farmaceutico.component";
 import {FarmaciaComponent} from "./components/farmacia/farmacia.component";
+import {VagaComponent} from "./components/vaga/vaga.component";
 
 export const routes: Routes = [
   {path: "farmaceutico", component: FarmaceuticoComponent},
   {path: "farmacia", component: FarmaciaComponent},
+  {path: "vagas", component: VagaComponent},
 ];
