100d92c58bd3fe88e298cb30cc6d42f851fb515f
vitor <vitorrf4@gmail.com>
2024-04-03 18:34:50 -0300

    atualiza requires

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express();
 const controller = require('../controllers/authController');
-const multer = require('multer');
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
 
-router.post('/auth/cadastro',
-            (req, res) => controller.cadastrarUsuario(req, res));
+router.post('/auth/cadastro', (req, res) => controller.cadastrarUsuario(req, res));
 router.post('/auth/login', (req, res) => controller.logarUsuario(req, res));
 
 module.exports = router;
