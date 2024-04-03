d8d2a7169d172e67decfd46c898288e98c029356
vitor <vitorrf4@gmail.com>
2024-04-03 09:29:10 -0300

    adiciona endpoint login

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -23,16 +23,46 @@ router.post('/cadastro', async (req, res) => {
     }
 
     const entidadeId = entidade.id;
-    const usuario = await Usuario.create({ email, senha: hashedSenha, tipo, entidadeId }); // Use the hashed password
+    const usuario = await Usuario.create({ email, senha: hashedSenha, tipo, entidadeId });
 
     return res.status(200).json(usuario);
 });
 
-router.post('/login', (req, res) => {
-});
+router.post('/login', async (req, res) => {
+    try {
+        const {email, senha} = req.body;
+        const usuario = await Usuario.findOne({where: {email: email}});
 
-router.get('/usuarios', async (req, res) => {
-    return res.status(200).json(await Usuario.findAll());
+        if (!usuario) {
+            return res.status(404).json({error: 'Credenciais inválidas'});
+        }
+
+        const senhaMatch = await bcrypt.compare(senha, usuario.senha);
+
+        if (!senhaMatch) {
+            return res.status(401).json({error: 'Credenciais inválidas'});
+        }
+        
+        delete usuario.dataValues.senha;
+        
+        const usuarioJson = usuario.dataValues;
+        
+        switch (usuarioJson.tipo) {
+            case 'EMPRESA':
+                const farmacia = await Farmacia.findByPk(usuario.entidadeId);
+                usuarioJson.informacoes = farmacia;
+                break;
+            case 'PESSOA':
+                const farmaceutico = await Farmaceutico.findByPk(usuario.entidadeId);
+                usuarioJson.informacoes = farmaceutico;
+                break;
+        }
+
+        return res.status(200).json(usuarioJson);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({error: 'Erro do servidor'});
+    }
 });
 
 module.exports = router;
