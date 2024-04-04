da4b129ccd46124ccaf4811e5edfa8e927d1147e
vitor <vitorrf4@gmail.com>
2024-04-03 22:41:02 -0300

    adiciona componente de vagas da empresa

---
+++ frontend/src/app/components/empresa/minhas-vagas/minhas-vagas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minhas-vagas',
+  standalone: true,
+  imports: [],
+  templateUrl: './minhas-vagas.component.html',
+  styleUrl: './minhas-vagas.component.css'
+})
+export class MinhasVagasComponent {
+
+}
