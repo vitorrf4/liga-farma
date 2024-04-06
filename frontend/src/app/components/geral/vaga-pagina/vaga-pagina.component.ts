37ae868ae025730160ba74319fc1f3c31111f356
vitor <vitorrf4@gmail.com>
2024-04-06 18:23:16 -0300

    desativa botao de candidatura para empresa

--- frontend/src/app/components/geral/vaga-pagina/vaga-pagina.component.ts
+++ frontend/src/app/components/geral/vaga-pagina/vaga-pagina.component.ts
@@ -2,6 +2,8 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Vaga} from "../../../models/vaga";
 import {DatePipe, NgIf} from "@angular/common";
 import {Router} from "@angular/router";
+import {LoginService} from "../../../services/login.service";
+import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 
 @Component({
   selector: 'app-vaga-pagina',
@@ -16,8 +18,16 @@ import {Router} from "@angular/router";
 export class VagaPaginaComponent {
   @Input() vaga: Vaga | null = null;
   @Output() vagaDeselecionadaEvent = new EventEmitter();
+  tipo: string | undefined = '';
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+              loginService: LoginService) {
+    this.tipo = loginService.usuario?.tipo;
+  }
+
+  get isPessoa() {
+    return this.tipo === 'PESSOA';
+  }
 
   async irParaCandidatura() {
     await this.router.navigateByUrl('candidatura', {state: {vaga: this.vaga}});
