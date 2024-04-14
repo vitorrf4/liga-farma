c4c1bcc30ef9053fcac157cc5a3963e1ffa21df1
vitor <vitorrf4@gmail.com>
2024-04-14 20:16:05 -0300

    ajusta environments

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  production: false,
   urlApi: "http://localhost:3000"
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  urlApi: "stable-phoenix-worthy.ngrok-free.app"
+};
