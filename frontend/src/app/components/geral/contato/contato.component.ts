87c20ea1ecba01a1d5b47729db884a8ce1c6fb37
vitor <vitorrf4@gmail.com>
2024-04-14 15:00:19 -0300

    ajusta interacao com o form de contato

--- frontend/src/app/components/geral/contato/contato.component.ts
+++ frontend/src/app/components/geral/contato/contato.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-contato',
@@ -8,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './contato.component.css'
 })
 export class ContatoComponent {
+  form: FormGroup;
 
+  constructor(private builder: FormBuilder) {
+    this.form = builder.group({
+      nome: [''],
+      sobrenome: [''],
+      email: [''],
+      telefone: [''],
+      mensagem: ['']
+    });
+  }
+
+  enviarMensagem() {
+    alert("Mensagem enviada com sucesso!");
+    this.form.reset();
+  }
 }
