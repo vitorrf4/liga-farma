3b96246088d6772a37ce8b5e0936b06b7e194996
vitor <vitorrf4@gmail.com>
2024-04-14 15:12:41 -0300

    protege rotas de usuario ja logado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -12,16 +12,18 @@ import {MinhasCandidaturasComponent} from "./components/pessoa/minhas-candidatur
 import {CriarVagaComponent} from "./components/empresa/criar-vaga/criar-vaga.component";
 import {EditarEmpresaComponent} from "./components/empresa/editar-empresa/editar-empresa.component";
 import {EditarPessoaComponent} from "./components/pessoa/editar-pessoa/editar-pessoa.component";
-import {authGuard} from "./misc/auth.guard";
+import {verificaLogadoGuard} from "./misc/verifica-logado.guard";
 import { HomeComponent } from './components/geral/home/home.component';
 import {somentePessoaGuard} from "./misc/somente-pessoa.guard";
 import {somenteEmpresaGuard} from "./misc/somente-empresa.guard";
 import { ContatoComponent } from './components/geral/contato/contato.component';
+import {verificaDeslogadoGuard} from "./misc/verifica-deslogado.guard";
 
 export const routes: Routes = [
   {path: "", pathMatch: 'full', redirectTo: 'home'},
-  {path: "login", component: LoginComponent},
-  {path: "cadastro", component: CadastroComponent, children: [
+  {path: "login", component: LoginComponent, canActivate: [verificaDeslogadoGuard], },
+  {path: "cadastro", component: CadastroComponent, canActivate: [verificaDeslogadoGuard],
+    children: [
       {path:'', redirectTo: 'pessoa', pathMatch: 'full',},
       {path:'pessoa', component: CadastroPessoaComponent},
       {path:'empresa', component: CadastroEmpresaComponent}
@@ -30,12 +32,12 @@ export const routes: Routes = [
   {path: "vagas", component: ListaVagasComponent},
   {path: "sobre-nos", component: SobreNosComponent},
   {path: "contato", component:ContatoComponent},
-  {path: "perfil", component: MeuPerfilComponent, canActivate: [authGuard]},
-  {path: "editar-e", component: EditarEmpresaComponent, canActivate: [authGuard, somenteEmpresaGuard]},
-  {path: "editar-p", component: EditarPessoaComponent, canActivate: [authGuard, somentePessoaGuard]},
-  {path: "minhas-vagas", component: MinhasVagasComponent, canActivate: [authGuard, somenteEmpresaGuard]},
-  {path: "criar-vaga", component: CriarVagaComponent, canActivate: [authGuard, somenteEmpresaGuard]},
-  {path: "candidatura", component: CandidaturaComponent, canActivate: [authGuard, somentePessoaGuard]},
-  {path: "minhas-candidaturas", component: MinhasCandidaturasComponent, canActivate: [authGuard, somentePessoaGuard]},
+  {path: "perfil", component: MeuPerfilComponent, canActivate: [verificaLogadoGuard]},
+  {path: "editar-e", component: EditarEmpresaComponent, canActivate: [verificaLogadoGuard, somenteEmpresaGuard]},
+  {path: "editar-p", component: EditarPessoaComponent, canActivate: [verificaLogadoGuard, somentePessoaGuard]},
+  {path: "minhas-vagas", component: MinhasVagasComponent, canActivate: [verificaLogadoGuard, somenteEmpresaGuard]},
+  {path: "criar-vaga", component: CriarVagaComponent, canActivate: [verificaLogadoGuard, somenteEmpresaGuard]},
+  {path: "candidatura", component: CandidaturaComponent, canActivate: [verificaLogadoGuard, somentePessoaGuard]},
+  {path: "minhas-candidaturas", component: MinhasCandidaturasComponent, canActivate: [verificaLogadoGuard, somentePessoaGuard]},
   {path: "**", redirectTo: ''}
 ];
--- frontend/src/app/misc/verifica-logado.guard.ts
+++ frontend/src/app/misc/verifica-logado.guard.ts
@@ -2,7 +2,7 @@ import {CanActivateFn, Router} from '@angular/router';
 import {inject} from "@angular/core";
 import {LoginService} from "../services/login.service";
 
-export const authGuard: CanActivateFn = async (route, state) => {
+export const verificaLogadoGuard: CanActivateFn = async (route, state) => {
     const estaLogado = inject(LoginService).estaLogado
     const router = inject(Router);
     if (estaLogado) {
