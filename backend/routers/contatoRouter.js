2216715bd25245ec627953ff20e0ac7576c21e55
vitor <vitorrf4@gmail.com>
2024-04-17 23:58:05 -0300

    ajusta router de contato

--- backend/routers/contatoRouter.js
+++ backend/routers/contatoRouter.js
@@ -2,21 +2,30 @@ const express = require('express');
 const router = express();
 const {verifyToken} = require("../config/jwt");
 const fs = require('fs');
+const path = require("node:path");
 
 router.use(verifyToken);
 
-router.post('/contato', (req, res) => {
+router.post('/contato', async (req, res) => {
     try {
-        let numeroArquivos;
-        fs.readdir('contrato', (err, files) => numeroArquivos = files);
-        const nomeArquivo = `contato_${numeroArquivos}`; 
-        
-        const writeStream = fs.createWriteStream(`contrato/${nomeArquivo}`);
-        writeStream.write(req.body);
-        writeStream.end();
+        const caminho = path.normalize(path.join(__dirname, '../', 'contato'));
+        await fs.readdir(caminho, (err, files) => {
+            if (err) {
+                console.log(err);
+                throw new Error();
+            }
+            const numeroArquivos = files.length | 0;
+            const nomeArquivo = `contato_${numeroArquivos}`;
             
+            const mensagem = JSON.stringify(req.body);
+
+            const writeStream = fs.createWriteStream(`${caminho}/${nomeArquivo}`);
+            writeStream.write(mensagem);
+            writeStream.end();
+        });
+
         return res.status(200).send('Mensagem recebida!');
-    } catch(err) {
+    } catch (err) {
         console.log(err);
         return res.status(500).send();
     }
