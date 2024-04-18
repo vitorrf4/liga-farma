9d2b9fa51ca735196758a43a6e0b6766c51e1695
vitor <vitorrf4@gmail.com>
2024-04-17 22:45:00 -0300

    adiciona router de contato

---
+++ backend/routers/contatoRouter.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express();
+const {verifyToken} = require("../config/jwt");
+const fs = require('fs');
+
+router.use(verifyToken);
+
+router.post('/contrato', (req, res) => {
+    try {
+        let numeroArquivos;
+        fs.readdir('contrato', (err, files) => numeroArquivos = files);
+        const nomeArquivo = `contato_${numeroArquivos}`; 
+        
+        const writeStream = fs.createWriteStream(`contrato/${nomeArquivo}`);
+        writeStream.write(req.body);
+        writeStream.end();
+            
+        return res.status(200).send('Mensagem recebida!');
+    } catch(err) {
+        console.log(err);
+        return res.status(500).send();
+    }
+});
+
+module.exports = router;
