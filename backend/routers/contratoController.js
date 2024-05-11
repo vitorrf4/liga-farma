07cf99b5679ae2782858e3db14d8cead0983ab7b
vitor <vitorrf4@gmail.com>
2024-05-11 17:35:48 -0300

    fix contato atualizar endpoint

--- backend/routers/contratoController.js
+++ backend/routers/contratoController.js
@@ -8,6 +8,6 @@ router.use(verifyToken);
 router.get('/', (req, res) => controller.getContratos(req, res));
 router.get('/:id', (req, res) => controller.getContratosPorId(req, res));
 router.post('/', (req, res) => controller.cadastrarContrato(req, res));
-router.put('/:id', (req, res) => controller.atualizarContrato(req, res));
+router.put('/', (req, res) => controller.atualizarContrato(req, res));
 
 module.exports = router;
