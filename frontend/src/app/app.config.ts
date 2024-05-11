e4174554d679cc1e32fce0c8fc45ce19f4b13b91
vitor <vitorrf4@gmail.com>
2024-05-11 16:01:23 -0300

    atualiza html de cadastro

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import {HttpClientModule, provideHttpClient, withInterceptors} from "@angular/common/http";
 import {JwtInterceptor} from "./misc/jwt.interceptor";
@@ -8,7 +7,6 @@ import {JwtInterceptor} from "./misc/jwt.interceptor";
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    // import da classe que fará as chamadas http
     importProvidersFrom(HttpClientModule),
     provideHttpClient(
       withInterceptors([JwtInterceptor])
