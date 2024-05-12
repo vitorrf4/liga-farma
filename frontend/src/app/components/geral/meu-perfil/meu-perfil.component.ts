30262435f460c4f1145fa18007a9b01f795d576d
vitor <vitorrf4@gmail.com>
2024-05-12 12:38:01 -0300

    adiciona componente contrato

--- frontend/src/app/components/geral/meu-perfil/meu-perfil.component.ts
+++ frontend/src/app/components/geral/meu-perfil/meu-perfil.component.ts
@@ -1,18 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {Usuario} from "../../../models/usuario";
 import {JsonPipe, NgForOf, NgIf} from "@angular/common";
-import {AuthService} from "../../../services/auth.service";
 import {PdfViewerModule} from "ng2-pdf-viewer";
-import {Farmaceutico} from "../../../models/farmaceutico";
 import {Router, RouterLink} from "@angular/router";
-import {Farmacia} from "../../../models/farmacia";
 import {LoginService} from "../../../services/login.service";
-import {PdfService} from "../../../services/pdf.service";
-import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 import {MinhasCandidaturasComponent} from "../../pessoa/minhas-candidaturas/minhas-candidaturas.component";
 import {MinhasVagasComponent} from "../../empresa/minhas-vagas/minhas-vagas.component";
 import {PerfilEmpresaComponent} from "../../empresa/perfil-empresa/perfil-empresa.component";
 import {PerfilPessoaComponent} from "../../pessoa/perfil-pessoa/perfil-pessoa.component";
+import {ContratosComponent} from "../../pessoa/contratos/contratos.component";
 
 @Component({
   selector: 'app-perfil',
@@ -26,7 +22,8 @@ import {PerfilPessoaComponent} from "../../pessoa/perfil-pessoa/perfil-pessoa.co
     MinhasCandidaturasComponent,
     MinhasVagasComponent,
     PerfilEmpresaComponent,
-    PerfilPessoaComponent
+    PerfilPessoaComponent,
+    ContratosComponent
   ],
   templateUrl: './meu-perfil.component.html',
   styleUrl: './meu-perfil.component.css'
---
+++ frontend/src/app/components/pessoa/contratos/contratos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contratos',
+  standalone: true,
+  imports: [],
+  templateUrl: './contratos.component.html',
+  styleUrl: './contratos.component.css'
+})
+export class ContratosComponent {
+
+}
